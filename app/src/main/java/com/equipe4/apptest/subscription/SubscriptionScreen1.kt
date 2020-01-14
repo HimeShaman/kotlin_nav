de2eb6f727df6ee6191f078abd0e321b8aa17716
Quimbert B <quimbenedicte@gmail.com>
2020-01-14 18:09:24 +0100

    Nav between subscription 1 and future screen with validator

--- app/src/main/java/com/equipe4/apptest/subscription/SubscriptionScreen1.kt
+++ app/src/main/java/com/equipe4/apptest/subscription/SubscriptionScreen1.kt
@@ -3,6 +3,10 @@ package com.equipe4.apptest.subscription
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Toast
+import com.equipe4.apptest.MainActivity
 import com.equipe4.apptest.R
 import kotlinx.android.synthetic.main.activity_subscription_screen1.*
 
@@ -13,17 +17,21 @@ class SubscriptionScreen1 : AppCompatActivity() {
         setContentView(R.layout.activity_subscription_screen1)
     }
 
-    fun onNext(){
-        val firstName:String = textInputEditText_firstName.toString()
-        val lastName:String = textInputEditText_lastName.toString()
-        if (firstName === ""){
+    fun goToSubscribe2(view: View){
+        val firstName:String = textInputEditText_firstName.text.toString()
+        val lastName:String = textInputEditText_lastName.text.toString()
+        if (firstName == ""){
             textInputEditText_firstName.error = getString(R.string.error_emptyInput)
-        } else if(lastName === ""){
-            textInputEditText_firstName.error = getString(R.string.error_emptyInput)
-        }else{
+        }
+        if(lastName == ""){
+            textInputEditText_lastName.error = getString(R.string.error_emptyInput)
+        }
+        if(lastName != "" && firstName!="")
+        {
             textInputEditText_firstName.error = null
             textInputEditText_lastName.error = null
-            val intent = Intent(this, SubscriptionScreen1::class.java)
+//            TODO : change intent to next screen when created instead of going back to Main
+            val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
         }
     }
