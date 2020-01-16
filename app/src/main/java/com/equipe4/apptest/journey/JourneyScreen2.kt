aeb09e71b5da6c96829712cbdbd7555cf14d4b65
IsautierEloise <eloise.isautier@hetic.net>
2020-01-16 17:37:48 +0100

    menu

--- app/src/main/java/com/equipe4/apptest/journey/JourneyScreen2.kt
+++ app/src/main/java/com/equipe4/apptest/journey/JourneyScreen2.kt
@@ -4,8 +4,14 @@ import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import com.equipe4.apptest.R
+import com.equipe4.apptest.UserProfileActivity
+import com.equipe4.apptest.messaging.MessagerieList
+import kotlinx.android.synthetic.main.activity_journey_screen1.*
 import kotlinx.android.synthetic.main.activity_journey_screen2.*
+import kotlinx.android.synthetic.main.activity_journey_screen2.bottom_bar
 
 class JourneyScreen2 : AppCompatActivity() {
 
@@ -29,6 +35,26 @@ class JourneyScreen2 : AppCompatActivity() {
             editText_input_searched_journey_start.setText(searchedJourney.startPoint)
             editText_input_searched_journey_end.setText(searchedJourney.endPoint)
         }
+
+        setSupportActionBar(bottom_bar)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        val inflater = menuInflater
+        inflater.inflate(R.menu.bottomappbar_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+
+        val profileIntent = Intent(this, UserProfileActivity::class.java)
+        val messageIntent = Intent(this, MessagerieList::class.java)
+
+        when (item!!.itemId) {
+            R.id.app_bar_profile -> startActivity(profileIntent)
+            R.id.app_bar_messages -> startActivity(messageIntent)
+        }
+        return true
     }
 
 }
