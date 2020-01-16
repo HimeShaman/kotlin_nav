e0defc0594125f208c5ec3dc3d75b20fbec954f6
IsautierEloise <eloise.isautier@hetic.net>
2020-01-16 16:44:51 +0100

    at least it displays

--- app/src/main/java/com/equipe4/apptest/MainActivity.kt
+++ app/src/main/java/com/equipe4/apptest/MainActivity.kt
@@ -3,8 +3,8 @@ package com.equipe4.apptest
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.Menu
 import android.view.View
+import android.view.Menu
 import com.equipe4.apptest.subscription.SubscriptionScreen1
 
 
@@ -12,18 +12,18 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        getActionBar()?.hide()
-        getSupportActionBar()?.hide()
-        //setSupportActionBar(findViewById(R.id.bottom_bar))
+
+        setActionBar(findViewById(R.id.bottom_bar))
+
         setContentView(R.layout.activity_main)
+
     }
 
-    /*override fun onCreateOptionsMenu(menu: Menu): Boolean {
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
         val inflater = menuInflater
         inflater.inflate(R.menu.bottomappbar_menu, menu)
         return true
-    }*/
-
+    }
     fun goToSubscribe1(view: View){
         val intent = Intent(this, SubscriptionScreen1::class.java)
         startActivity(intent)
